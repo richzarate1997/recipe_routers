76677dce686cb5a00e05db93ea318b5bcd015c75
richzarate1997 <richzarate97@gmail.com>
2023-06-28 14:18:15 -0700

    ingredientjdbc repo

--- agile_aprons/src/main/java/learn/agileaprons/data/IngredientRepository.java
+++ agile_aprons/src/main/java/learn/agileaprons/data/IngredientRepository.java
@@ -7,6 +7,8 @@ import java.util.List;
 public interface IngredientRepository {
     List<Ingredient> findAll();
     Ingredient findById(int id);
-    Ingredient findByName(String name);
+    List<Ingredient> findByName(String name);
     Ingredient create(Ingredient ingredient);
+    boolean update(Ingredient ingredient);
+    boolean deleteById(int id);
 }
