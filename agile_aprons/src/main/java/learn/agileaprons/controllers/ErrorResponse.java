bcd45a34579bae5bce89fa9ec7525d70401d793d
Alice Wu <alicewu@Alices-Air.T-mobile.com>
2023-06-29 09:27:35 -0500

    Fixed small error response bug and test naming

--- agile_aprons/src/main/java/learn/agileaprons/controllers/ErrorResponse.java
+++ agile_aprons/src/main/java/learn/agileaprons/controllers/ErrorResponse.java
@@ -8,9 +8,9 @@ import org.springframework.http.ResponseEntity;
 public class ErrorResponse {
     public static <T> ResponseEntity<Object> build(Result<T> result) {
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-        if (result.getType() == null || result.getType() == ResultType.INVALID) {
+        if (result.getResultType() == null || result.getResultType() == ResultType.INVALID) {
             status = HttpStatus.BAD_REQUEST;
-        } else if (result.getType() == ResultType.NOT_FOUND) {
+        } else if (result.getResultType() == ResultType.NOT_FOUND) {
             status = HttpStatus.NOT_FOUND;
         }
         return new ResponseEntity<>(result.getMessages(), status);
--- agile_aprons/src/test/java/learn/agileaprons/data/AppUserJdbcTemplateRepositoryTest.java
+++ agile_aprons/src/test/java/learn/agileaprons/data/AppUserJdbcTemplateRepositoryTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-class AppUserJdbcTemplateRepositoryRepositoryTest {
+class AppUserJdbcTemplateRepositoryTest {
 
     @Autowired
     JdbcTemplate jdbcTemplate;
