a0408cafeb9050ac4d00437f00c2f63b58d251c9
Alice Wu <alicewu@Alices-Air.T-mobile.com>
2023-06-30 16:06:43 -0500

    Stubbing out all the pages and adding routes

--- clients/client-web/src/App.js
+++ clients/client-web/src/App.js
@@ -1,16 +1,25 @@
 import Home from "./components/Home";
 import ResponsiveAppBar from "./components/ResponsiveAppBar";
 import Recipe from "./components/Recipe";
+import About from "./components/About"
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+import Footer from "./components/Footer";
 
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
     <>
-      <ResponsiveAppBar />
       <Router>
+        <ResponsiveAppBar />
+        <Footer />
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/recipes" element={<Recipe />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Recipes" element={<Recipe />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/" element={<Home />}/>
         </Routes>
       </Router>
 
---
+++ clients/client-web/src/components/Profile.js
@@ -0,0 +1,6 @@
+
+function Profile(){
+    return <h2>profile</h2>;
+}
+
+export default Profile;
