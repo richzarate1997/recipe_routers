ebeca5836e6f1b13c902043d06f4975a9a7717f3
richzarate1997 <richzarate97@gmail.com>
2023-07-01 15:42:40 -0700

    layout for user service and tests

---
+++ agile_aprons/src/test/java/learn/agileaprons/domain/UserServiceTest.java
@@ -0,0 +1,56 @@
+package learn.agileaprons.domain;
+
+import learn.agileaprons.data.DataException;
+import learn.agileaprons.data.UserRepository;
+import learn.agileaprons.models.Recipe;
+import learn.agileaprons.models.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class UserServiceTest {
+
+    @MockBean
+    UserRepository repository;
+
+    @Autowired
+    UserService service;
+
+    @Test
+    void shouldFindUserById() {
+        User user = makeUser();
+        user.setId(1);
+
+        when(repository.findById(1)).thenReturn(user);
+
+        User actual = service.findById(1);
+
+        assertEquals(user, actual);
+    }
+
+    @Test
+    void shouldCreateUser() throws DataException {
+        User user = makeUser();
+        User expectedUser = makeUser();
+        expectedUser.setId(3);
+
+        when(repository.create(user)).thenReturn(expectedUser);
+        Result<User> actual = service.create(user);
+        assertTrue(actual.isSuccess());
+        assertNotNull(actual.getPayload());
+        assertEquals(expectedUser, actual.getPayload());
+    }
+
+    private User makeUser(){
+        User user = new User();
+        user.setId(3);
+        user.setDisplayName("Jim");
+        user.setMetric(false);
+        return user;
+    }
+}
