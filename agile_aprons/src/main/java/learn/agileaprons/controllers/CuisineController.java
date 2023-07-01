afa2fcfa71445b271b1fa1c5d082ae3abbbbafd8
richzarate1997 <richzarate97@gmail.com>
2023-07-01 09:57:26 -0700

    cuisine controller added

---
+++ agile_aprons/src/main/java/learn/agileaprons/controllers/CuisineController.java
@@ -0,0 +1,26 @@
+package learn.agileaprons.controllers;
+
+import learn.agileaprons.domain.CuisineService;
+import learn.agileaprons.models.Cuisine;
+import learn.agileaprons.models.Unit;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/cuisine")
+public class CuisineController {
+
+    private final CuisineService service;
+
+    public CuisineController(CuisineService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Cuisine> findAll() {return  service.findAll();}
+}
