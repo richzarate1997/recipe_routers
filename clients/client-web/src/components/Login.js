da92bd13d2871cd17f3c0816f83d7ca4f81f65be
Alice Wu <alicewu@Alices-Air.T-mobile.com>
2023-07-02 00:07:49 -0500

    finished login page and added auth related files

--- clients/client-web/src/components/Login.js
+++ clients/client-web/src/components/Login.js
@@ -1,6 +1,104 @@
+import { useContext, useState } from "react";
+import { useNavigate} from "react-router";
+import { Link } from "react-router-dom";
+import AuthContext from "../contexts/AuthContext";
+import { authenticate } from "../service/authApi";
+import { TextField, Avatar, Button, FormControlLabel, Container, Box, Grid, Checkbox, Typography } from "@mui/material";
+import Errors from "./Errors";
+import LockOutlinedIcon  from "@mui/icons-material/LockOutlined";
+import CssBaseline from "@mui/material/CssBaseline";
+function Login() {
+    const [errors, setErrors] = useState([]);
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+    });
 
-function Login(){
-    return <h2>login</h2>;
+    const navigate = useNavigate();
+    const auth = useContext(AuthContext);
+
+    const handleChange = (event) => {
+        const nextCredentials = { ...credentials };
+        nextCredentials[event.target.name] = event.target.value;
+        setCredentials(nextCredentials);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        authenticate(credentials).then(user => {
+            auth.onAuthenticated(user);
+            navigate(-1); // might wanna make sure this works c:
+        })
+            .catch(error => setErrors(error));
+    };
+    // TODO attach handlers to form functions
+    return (
+        <Container component="main" maxWidth="xs" >
+            <CssBaseline />
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Avatar sx={{ m: 1, bgcolor: '#612D33' }}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign In
+                </Typography>
+                <Box component="form" sx={{ mt: 1 }}>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Sign In
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="#" variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+            <Errors errors={errors} />
+        </Container>
+    )
 }
 
 export default Login;
---
+++ clients/client-web/src/service/authApi.js
@@ -0,0 +1,65 @@
+const url = 'http://localhost:8080/security';
+
+export async function authenticate(credentials) {
+    const init = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    };
+
+    const response = await fetch(`${url}/authenticate`, init);
+    if (response.status === 200){
+        const data = await response.json();
+        return makeUser(data);
+    } else {
+        return Promise.reject('Bad crednetials');
+    }
+}
+
+export async function refreshToken() {
+    const jwtToken = localStorage.getItem('jwt_token');
+
+    const init = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${jwtToken}`
+        }
+    };
+
+    const response = await fetch(`${url}/refresh-token`, init);
+
+    if(response.status === 200){
+        const data = await response.json();
+        return makeUser(data);
+    } else {
+        signOut();
+        return Promise.reject('Session expired');
+    }
+}
+
+export function signOut() {
+    localStorage.removeItem('jwt_token');
+}
+
+const makeUser = (authResponse) => {
+    const jwtToken = authResponse.jwt_token;
+    localStorage.setItem('jwt_token', jwtToken);
+    return makeUserFromJwt(jwtToken);
+};
+
+const makeUserFromJwt = (jwtToken) => {
+    const tokenParts = jwtToken.split('.');
+    if(tokenParts.length > 1){
+        const userData = tokenParts[1];
+        const decodedUserData = JSON.parse(atob(userData));
+        return {
+            appUserId: decodedUserData.app_user_id,
+            username: decodedUserData.sub,
+            roles: decodedUserData.authorities.split(',')
+        }
+    }
+};
