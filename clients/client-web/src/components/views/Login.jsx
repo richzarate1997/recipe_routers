e5ede144214d119a79b2dd4f352e00388a85b609
richzarate1997 <richzarate97@gmail.com>
2023-07-03 13:58:43 -0700

    added useEffect

--- clients/client-web/src/components/views/Login.jsx
+++ clients/client-web/src/components/views/Login.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 import { Link, useLocation } from "react-router-dom";
 import AuthContext from "../../contexts/AuthContext";
