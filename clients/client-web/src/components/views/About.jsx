e6a80affb8a44d3f5452983c0e0f9e45151d6335
Alice Wu <alicewu@Alices-Air.T-mobile.com>
2023-07-04 11:05:56 -0500

    Finished up about page and recipe cards

--- clients/client-web/src/components/views/About.jsx
+++ clients/client-web/src/components/views/About.jsx
@@ -1,40 +1,49 @@
-import { Grid, Typography} from "@mui/material";
+import { Divider, Grid, Typography} from "@mui/material";
 import ProfileCard from "../ProfileCard";
 import { Container } from "@mui/system";
-import CssBaseline from "@mui/material/CssBaseline";
+import alice from "../../assets/alice.jpg";
+import kern from "../../assets/kern.jpg";
+import richard from "../../assets/richard.jpg"
 
+const aboutblurb = "GetYum is a recipe-to-shopping list application that is built on Spoonacular API, a React UI, and Java with MySQL server and database, built by our team, React Routers. The goal of GetYum is to create an all-in-one space for browsing, creating, and saving recipes such that you have somewhere to directly create a grocery list that has all the items you need to make your recipes. With GetYum, you'll no longer have to make multiple trips to the grocery store to pick up that one ingredient you missed for your favorite recipe. \n\nRecipe Routers is a team made from members of Dev10's cohort 55. The members of Recipe Routers come from different backgrounds and partook in a fast paced full stack Java training; GetYum is our final capstone for this program.";
 
 function About() {
     
     return (
         <Container>
-            <CssBaseline />
-            <Typography variant="h3">About Us</Typography>
-            <Typography variant="body1">text</Typography>
-            <Typography variant="h3">Meet the Team</Typography>
+            
+            <Typography variant="h3" pt={2}>About Us</Typography>
+            <Divider></Divider>
+            <Typography variant="body1" sx={{
+                padding: 3,
+                fontSize: 20,
+                lineHeight: 2.5
+            }}>{aboutblurb}</Typography>
+            <Typography variant="h3" pt={2}>Meet the Routers</Typography>
+            <Divider></Divider>
             <Grid container spacing={2} mx='auto' my={2}>
                 <Grid item xs={12} sm={6} md={4}>
                 <ProfileCard 
-                    imgUrl=""
-                    imgDesc="a picture of Alice Wu"
-                    name="Alice Wu"
-                    description="A recent graduate from UIUC"
+                    imgUrl={kern}
+                    imgDesc="a picture of Coren 'Kern' Frankel"
+                    name="Coren 'Kern' Frankel"
+                    description="An avid puzzle-solver and cinephile, Kern likes to spend his free time waxing philosophical with his wife and arguing about the meaning of life."
                 />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
                 <ProfileCard 
-                    imgUrl=""
+                    imgUrl={alice}
                     imgDesc="a picture of Alice Wu"
                     name="Alice Wu"
-                    description="A recent graduate from UIUC"
+                    description="Rocking a bowl cut and circular glasses for half of her life, Alice enjoys building keyboards, playing games, and cooking in her spare time. "
                 />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
                 <ProfileCard 
-                    imgUrl=""
-                    imgDesc="a picture of Alice Wu"
-                    name="Alice Wu"
-                    description="A recent graduate from UIUC"
+                    imgUrl={richard}
+                    imgDesc="a picture of Richard Zarate"
+                    name="Richard Zarate"
+                    description="If you know Richard you know he's always on the go. Keeping an active body and mind is the goal. You can usually find him coding, traveling or walking his dog Niko"
                 />
                 </Grid>
                 
--- clients/client-web/src/components/views/Recipe.jsx
+++ clients/client-web/src/components/views/Recipe.jsx
@@ -1,14 +1,121 @@
-import { Box } from "@mui/system";
+import { Box, Grid, Tooltip } from "@mui/material";
 import RecipeCard from "../RecipeCard";
+import { useEffect, useState } from "react";
+import { searchRecipes } from "../../service/fetchApi";
+import Errors from "../Errors";
+
+
+const RECIPE_DEFAULT = {
+    "id": 0,
+    "image": "",
+    "readyInMinutes": "",
+    "servings": 0,
+    "sourceUrl": "",
+    "title": ""
+}
+
+
+const BASE_RECIPES = [
+    {"id": 491786,
+    "image": "Ranch-BLT-Pasta-Salad-491786.jpg",
+    "readyInMinutes": 45,
+    "servings": 8,
+    "sourceUrl": "https://www.cinnamonspiceandeverythingnice.com/ranch-blt-pasta-salad/",
+    "title": "Ranch BLT Pasta Salad"},
+
+    {"id": 492413,
+    "image": "Chicken-Gnocchi-Soup-(Olive-Garden-Copycat)-492413.jpg",
+    "readyInMinutes": 65,
+    "servings": 6,
+    "sourceUrl": "http://www.cinnamonspiceandeverythingnice.com/olive-garden-chicken-and-gnocchi-soup-copycat/",
+    "title": "Chicken Gnocchi Soup (Olive Garden Copycat)"},
+    
+    {"id": 909570,
+    "image": "soy-sauce-noodles-909570.jpg",
+    "readyInMinutes": 30,
+    "servings": 2,
+    "sourceUrl": "http://www.loveandoliveoil.com/2015/03/soy-sauce-noodles.html",
+    "title": "Soy Sauce Noodles"},
+
+    {"id": 667384,
+    "image": "Beef-orzo-skillet-667384.jpg",
+    "readyInMinutes": 45,
+    "servings": 4,
+    "sourceUrl": "http://www.sheknows.com/food-and-recipes/articles/1027283/beef-orzo-skillet-recipe",
+    "title": "Beef orzo skillet"},
+   
+    {"id": 709814,
+    "image": "roasted-ratatouille-pasta-709814.jpg",
+    "readyInMinutes": 63,
+    "servings": 3,
+    "sourceUrl": "http://thewoksoflife.com/2014/03/ratatouille-pasta/",
+    "title": "Roasted Ratatouille Pasta"},
+    
+    {"id": 750949,
+    "image": "mexican-macaroni-salad-750949.jpeg",
+    "readyInMinutes": 60,
+    "servings": 12,
+    "sourceUrl": "http://www.foodnetwork.com/recipes/ree-drummond/mexican-macaroni-salad.html",
+    "title": "Mexican Macaroni Salad"},
+    
+   {"id": 718981,
+    "image": "cheeseburger-gnocchi-718981.jpg",
+    "readyInMinutes": 45,
+    "servings": 4,
+    "sourceUrl": "http://www.kevinandamanda.com/recipes/dinner/cheeseburger-gnocchi.html",
+    "title": "Cheeseburger Gnocchi"},
+   
+    {"id": 864561,
+    "image": "mediterranean-pasta-salad-864561.jpg",
+    "readyInMinutes": 25,
+    "servings": 8,
+    "sourceUrl": "http://littlespicejar.com/mediterranean-pasta-salad/",
+    "title": "Mediterranean Pasta Salad"},
+
+    {"id": 494242,
+    "image": "Pumpkin---Ricotta-Stuffed-Shells-494242.jpg",
+    "readyInMinutes": 45,
+    "servings": 10,
+    "sourceUrl": "http://www.loveandoliveoil.com/2013/02/pumpkin-ricotta-stuffed-shells.html",
+    "title": "Pumpkin & Ricotta Stuffed Shells"},
+    
+    {"id": 725830,
+    "image": "bas-best-baked-ziti-725830.jpg",
+    "readyInMinutes": 45,
+    "servings": 6,
+    "sourceUrl": "http://www.bonappetit.com/recipe/bas-best-baked-ziti",
+    "title": "BA’s Best Baked Ziti"}]
+
 function Recipe() {
+    const [recipes, setRecipes] = useState([]);
+    const [errors, setErrors] = useState([]);
+    
+
+    // useEffect(() => {
+    //     searchRecipes("pasta")
+    //     .then(data => {
+    //         //console.log(data.results);
+    //         setRecipes(data.results)
+    //     })
+    //     .catch(err => setErrors(err))
+    // }, []);
+
+
+
     return (
         <Box mx={4} sx={{paddingTop: 2}}>
-            <RecipeCard  imgUrl="https://www.indianhealthyrecipes.com/wp-content/uploads/2023/05/red-sauce-pasta-recipe.jpg" 
-            imgDesc="Tomato sauce rotinin pasta" 
-            name="Red Sauce Pasta" 
-            description="Craving a simple, cozy pasta? Try this Classic Red Sauce Pasta recipe! Like any great Italian recipe, it features simple, classic ingredients simmered until they meld into a savory, velvety tomato sauce. This one is based on a real Italian grandmother’s recipe (so you know it’s good!). Throw it onto pasta with a little salty Parmesan or Pecorino Romano cheese, and it’s simply divine. This one went over very well in our house: each of us huddled over our cozy bowls…and refusing to share!"
-            cookTime="30 min"
-            />
+            <Grid container spacing={2} mx='auto' my={2}>
+            {BASE_RECIPES.map(recipe => (
+                    
+                    <Grid item xs={12} sm={6} md={4} py={2}>
+                        <Tooltip title={recipe.title}>
+                        <RecipeCard imgUrl={recipe.image} name={recipe.title} cookTime={recipe.readyInMinutes} servings={recipe.servings} />
+                        </Tooltip>
+                </Grid>
+                
+            ))}
+            </Grid>
+            <Errors errors={errors} />
         </Box>
     );
 }
