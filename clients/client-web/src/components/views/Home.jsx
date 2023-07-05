367837dfa516156e6beeafff12863eb5e0086429
unclebabykern <coren.frankel@gmail.com>
2023-07-05 00:20:41 -0600

    refactored SearchBar to unpack extra components to Home view

--- clients/client-web/src/components/views/Home.jsx
+++ clients/client-web/src/components/views/Home.jsx
@@ -1,11 +1,36 @@
 import SearchBar from "../SearchBar";
 import FunFact from "../FunFact";
+import logo from "../../assets/logo.png";
+import { Box } from '@mui/material'
 
 function Home() {
+    const styles = {
+        box: {
+            display: "flex",
+            alignSelf: "center",
+            justifyContent: "center",
+            flexDirection: "column"
+        },
+        logo: {
+            maxWidth: '20%',
+            display: "flex",
+            alignSelf: "center",
+            justifyContent: "center",
+            flexDirection: "column"
+        }
+    }
+
     return (
         <>
-            <SearchBar />
-            <FunFact />
+            <Box>
+                <Box pt={8} style={styles.box}>
+                    <img src={logo} alt="hambuger logo" style={styles.logo} />
+                </Box>
+                <Box px={10} style={styles.box}>
+                    <SearchBar />
+                </Box>
+                <FunFact />
+            </Box>
         </>
     );
 }
