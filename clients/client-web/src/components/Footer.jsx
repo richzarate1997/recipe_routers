ab69c44ee7b7d7ab44d989f0962f175a3663417a
unclebabykern <coren.frankel@gmail.com>
2023-07-04 19:05:07 -0600

    Style player with color scheme 1

--- clients/client-web/src/components/Footer.jsx
+++ clients/client-web/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer style={{color: "gray", position: "fixed", bottom: 0, right:20, fontFamily: "monospace"}}>
+        <footer style={{color: "#612D33", position: "fixed", bottom: 0, right:20, fontFamily: "monospace"}}>
             <p>Copyright @ GetYum 2023</p>
         </footer>
     )
