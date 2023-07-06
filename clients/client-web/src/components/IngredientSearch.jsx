eb725e3544a1771ab90815eda3afc729d9337235
richzarate1997 <richzarate97@gmail.com>
2023-07-05 21:17:43 -0700

    ingredient list component

--- clients/client-web/src/components/IngredientSearch.jsx
+++ clients/client-web/src/components/IngredientSearch.jsx
@@ -1,54 +1,8 @@
-
 import { findIngredientByName } from "../service/ingredientApi";
 import { useState, useEffect } from "react";
-import SearchIcon from '@mui/icons-material/Search';
-import TextField from "@mui/material/TextField";
-import { Box, Button, InputBase, Grid, Autocomplete } from '@mui/material';
-import { styled, alpha } from '@mui/material/styles';
-import { useNavigate } from "react-router-dom";
+import { Box, TextField, Grid, Autocomplete } from '@mui/material';
 
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(1),
-        width: 'auto',
-    },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-        padding: theme.spacing(1, 1, 1, 0),
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            width: '12ch',
-            '&:focus': {
-                width: '20ch',
-            },
-        },
-    },
-}));
-
-const IngredientSearch = () => {
+const IngredientAutoComplete = () => {
     const [searchQuery, setSearchQuery] = useState('');
     const [searchResults, setSearchResults] = useState([]);
     const [selectedIngredients, setSelectedIngredients] = useState([]);
@@ -76,41 +30,32 @@ const IngredientSearch = () => {
         setSearchQuery('');
     };
 
-    const fetchIngredients = () => {
-        findIngredientByName(searchQuery)
-            .then(data => {
-                console.log(data);
-            })
-            .catch(error => {
-
-                console.error(error);
-            });
-    };
-
-
     return (
-        <Box m={1} sx={{ display: 'flex' }} component='form' onSubmit={fetchIngredients}>
-            <Search>
-                <SearchIconWrapper>
-                    <SearchIcon />
-                </SearchIconWrapper>
-                {/* <Autocomplete
+        <Box m={1} sx={{display: 'flex'}} component='form'>
+            <Autocomplete
                 freeSolo
                 options={searchResults}
-                getOptionLabel={(option) => option.name} */}
-                <StyledInputBase
-                    placeholder="Search…"
-                    inputProps={{ 'aria-label': 'search' }}
-                    value={searchQuery}
-                    onChange={handleSearch}
-                // />
-                />
-            </Search>
-            <Button type='submit' variant="contained" color="secondary" sx={{ mr: 2 }}>
-                Search
-            </Button>
+                getOptionLabel={(option) => option.name}
+                inputValue={searchQuery}
+                onInputChange={handleSearch}
+                onChange={handleSelect}
+                renderInput={(params) => 
+                    <TextField {...params} 
+                        placeholder="Search…" 
+                        inputProps={{ ...params.inputProps, 'aria-label': 'search' }}
+                    />}
+            />
+            <Grid container spacing={2}>
+                {selectedIngredients.map((ingredient, index) => (
+                    <Grid item key={index}>
+                        <Box>
+                            {ingredient.name}
+                        </Box>
+                    </Grid>
+                ))}
+            </Grid>
         </Box>
     );
 }
 
-export default IngredientSearch;
+export default IngredientAutoComplete;
--- clients/client-web/src/components/RecipeFormStep2.jsx
+++ clients/client-web/src/components/RecipeFormStep2.jsx
@@ -2,12 +2,13 @@ import { Fragment } from 'react'
 import { Link } from 'react-router-dom';
 import { Typography, Button } from '@mui/material';
 import IngredientSearch from "./IngredientSearch"
+import IngredientList from './IngredientList';
 
 const RecipeFormStep2 = ({ ingredients, handleIngredientChange, header }) => (
     <Fragment>
         <Typography variant="h4" p={2}>{header}</Typography>
         {<Fragment>
-            <IngredientSearch />
+            <IngredientList />
             <Typography variant="h5" p={1}>Or</Typography>
             <Button component={Link} to="/ingredient" variant="contained" color="primary">
                 Add Ingredient
