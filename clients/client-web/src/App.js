590453e24bdc662ba7a36d1a9ed16f7eff553adf
unclebabykern <coren.frankel@gmail.com>
2023-07-26 14:08:21 -0600

    Undo changes to facilitate merge

--- clients/client-web/src/App.js
+++ clients/client-web/src/App.js
@@ -20,104 +20,121 @@ import FavoriteRecipesList from "./components/FavoriteRecipesList";
 
 
 const EMPTY_USER = {
-  username: '',
-  roles: []
+    username: '',
+    roles: []
 };
 
 const WAIT_TIME = 1000 * 60 * 14;
 
 function App() {
-  const [user, setUser] = useState(EMPTY_USER);
+    const [user, setUser] = useState(EMPTY_USER);
+    
+    const auth = {
+        user: user,
+        isLoggedIn() {
+            return !!user.username;
+        },
+        hasRole(role) {
+            return user.roles.includes(role);
+        },
+        onAuthenticated(user) {
+            setUser(user);
+            setTimeout(refreshUser, WAIT_TIME);
+        },
+        signOut() {
+            setUser(EMPTY_USER);
+            signOut();
+        }
+    };
+    
+    const refreshUser = useCallback(() => {
+        refreshToken()
+            .then(existingUser => {
+                setUser(existingUser);
+                setTimeout(refreshUser, WAIT_TIME);
+            })
+            .catch(err => {
+                console.log(err);
+                auth.signOut();
+            });
+    }, []);
 
-  const auth = {
-    user: user,
-    isLoggedIn() {
-      return !!user.username;
-    },
-    hasRole(role) {
-      return user.roles.includes(role);
-    },
-    onAuthenticated(user) {
-      setUser(user);
-      setTimeout(refreshUser, WAIT_TIME);
-    },
-    signOut() {
-      setUser(EMPTY_USER);
-      signOut();
-    }
-  };
+    useEffect(() => {
+        refreshUser();
+    }, [refreshUser]);
 
-  const refreshUser = useCallback(() => {
-    if (auth.user.username) {
-      refreshToken()
-        .then(existingUser => {
-          setUser(existingUser);
-          setTimeout(refreshUser, WAIT_TIME);
-        })
-        .catch(() => auth.signOut());
-    }
-  }, []);
+    const theme = createTheme({
+        palette: {
+            mode: 'light',
+            primary: {
+                main: '#7CA65A',
+                contrastText: '#fff'
+            },
+            secondary: {
+                main: "#FEAE65"
+            },
+            warning: {
+                main: '#CA5953'
+            },
+            danger: {
+                main: '#612D33'
+            },
+            info: {
+                main: '#D1483D'
+            }
+        },
+    });
 
-  useEffect(() => {
-    refreshUser();
-  }, [refreshUser]);
-
-  const theme = createTheme({
-    palette: {
-      mode: 'light',
-      primary: {
-        main: '#7CA65A',
-        contrastText: '#fff'
-      },
-      secondary: {
-        main: "#FEAE65"
-      },
-      warning: {
-        main: '#CA5953'
-      },
-      danger: {
-        main: '#612D33'
-      },
-      info: {
-        main: '#D1483D'
-      }
-    },
-  });
-
-  const mayRedirect = (happyPath, sadPath) => {
-    return auth.isLoggedIn() ? happyPath : sadPath;
-  }
-
-  return (
-    <ThemeProvider theme={theme}>
-      <AuthContext.Provider value={auth}>
-        <Router>
-          <ResponsiveAppBar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/recipes" element={<Recipe />} />
-            <Route path="/search/:param" element={<Recipe />} />
-            <Route path="/recipe/:id" element={<ShowRecipe userId={user.appUserId} />} />
-            <Route path="/add/ingredient" element={<AddGroceries />} />
-            <Route path="/new/recipe" element={mayRedirect(<RecipeForm />, <Navigate to="/recipes" />)} />
-            <Route path="/add/grocerylist" element={mayRedirect(<GroceryListForm />, <Navigate to="/recipes" />)} />
-            <Route path="/myfavorites" element={mayRedirect(<FavoriteRecipesList />, <Navigate to="/recipes" />)} />
-            <Route path="/about" element={<About />} />
-            <Route path="/profile" element={mayRedirect(<Profile appUser={auth.user} />, <Navigate to='/' />)} />
-            <Route path="/login" element={
-              mayRedirect(<Navigate to='/profile' />, <Login heading="Sign In" buttonText="Sign In" />)
-            } />
-            <Route path="/register" element={
-              mayRedirect(<Navigate to='/profile' />, <Login heading="Register" buttonText="Register" isRegistration={true} />)
-            } />
-            <Route path="/notfound" element={<NotFound />} />
-            <Route path="*" element={<Navigate to={'/notfound'} />} />
-          </Routes>
-          <Footer />
-        </Router>
-      </AuthContext.Provider>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <AuthContext.Provider value={auth}>
+                <Router>
+                    <ResponsiveAppBar />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/recipes" element={<Recipe />} />
+                        <Route path="/search/:param" element={<Recipe />} />
+                        <Route path="/recipe/:id" element={<ShowRecipe userId={user.appUserId}/>} />
+                        <Route path="/add/ingredient" element={<AddGroceries />} />
+                        <Route path="/new/recipe" element={
+                            auth.isLoggedIn()
+                                ? <RecipeForm />
+                                : <Navigate to="/recipes" />
+                        } />
+                        <Route path="/add/grocerylist" element={
+                            auth.isLoggedIn()
+                                ? <GroceryListForm />
+                                : <Navigate to="/recipes" />
+                        } />
+                        <Route path="/myfavorites" element={
+                            auth.isLoggedIn()
+                                ? <FavoriteRecipesList />
+                                : <Navigate to="/recipes" />
+                        } />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/profile" element={
+                            auth.isLoggedIn()
+                                ? <Profile appUser={auth.user} />
+                                : <Navigate to='/' />
+                        } />
+                        <Route path="/login" element={
+                            auth.isLoggedIn()
+                                ? <Navigate to='/profile' />
+                                : <Login heading="Sign In" buttonText="Sign In" />
+                        } />
+                        <Route path="/register" element={
+                            auth.isLoggedIn()
+                                ? <Navigate to='/profile' />
+                                : <Login heading="Register" buttonText="Register" isRegistration={true} />
+                        } />
+                        <Route path="/notfound" element={<NotFound />} />
+                        <Route path="*" element={<Navigate to={'/notfound'} />} />
+                    </Routes>
+                    <Footer />
+                </Router>
+            </AuthContext.Provider>
+        </ThemeProvider>
+    );
 }
 
 export default App;
