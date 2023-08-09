f18d2b74265488e199ac006b264bd2f2103e883b
unclebabykern <coren.frankel@gmail.com>
2023-08-09 14:00:20 -0600

    Adapt recipe properties to new norm & reformat

--- clients/client-web/src/components/RecipeCard.jsx
+++ clients/client-web/src/components/RecipeCard.jsx
@@ -8,69 +8,69 @@ import MenuBookOutlinedIcon from '@mui/icons-material/MenuBookOutlined';
 import { scrapeRecipe } from '../service/recipeApi';
 import { useNavigate } from 'react-router-dom';
 
-const imageUrl = "https://spoonacular.com/recipeImages/";
+const imageBase = "https://spoonacular.com/recipeImages/";
 
 
-export default function RecipeCard({ id, imgUrl, name, cookTime, servings }) {
+export default function RecipeCard({ id, imageUrl, title, cookMinutes, servings }) {
   const navigate = useNavigate();
   const recipe = {
-      id: id,
-      title: name,
-      servings: servings,
-      cookMinutes: cookTime
+    id: id,
+    title: title,
+    servings: servings,
+    cookMinutes: cookMinutes
   };
-  const renderImage = () => imgUrl.includes(imageUrl) ? imgUrl : imageUrl + imgUrl;
-  
+  const renderImage = () => imageUrl.includes(imageBase) ? imageUrl : imageBase + imageUrl;
+
   const getRecipe = () => {
-      scrapeRecipe(recipe)
-          .then(data => navigate(`/recipe/${data.id}`))
-          .catch(err => console.log(err));
+    scrapeRecipe(recipe)
+      .then(data => navigate(`/recipe/${data.id}`))
+      .catch(err => console.log(err));
   }
   return (
-      <CardActionArea sx={{ width: 345 }}>
-          <Tooltip title={name} placement='top' arrow>
-              <Card
-                  sx={{ width: 345, height: 370 }}
-                  py={2} 
-                  onClick={() => getRecipe()}
-              >
-                  <CardMedia
-                      component="img"
-                      height="175"
-                      image={renderImage()}
-                      alt={name} />
-                  <CardContent>
-                      <Typography gutterBottom variant="h5" component="div" textAlign={'center'}>
-                          {name}
-                      </Typography>
-                      <Grid container px={2} pt={3} >
-                          <Grid item xs={12} sm={6} md={4}
-                              sx={{
-                                  display: 'flex',
-                                  alignItems: 'center',
-                                  flexWrap: 'wrap',
-                                  marginRight: 6,
-                                  justifyContent: 'center'
-                              }}>
-                              <AccessTimeOutlinedIcon />
-                              <Typography>{cookTime} minutes</Typography>
-                          </Grid>
-                          <Grid item xs={12} sm={6} md={4}
-                              sx={{
-                                  display: 'flex',
-                                  alignItems: 'center',
-                                  flexWrap: 'wrap',
-                                  justifyContent: 'center'
-                              }}>
-                              <Box sx={{ justifyContent: 'center' }}>
-                                  <MenuBookOutlinedIcon />
-                              </Box>
-                              <Typography>{servings} servings</Typography>
-                          </Grid>
-                      </Grid>
-                  </CardContent>
-              </Card>
-          </Tooltip>
-      </CardActionArea>
+    <CardActionArea sx={{ width: 345 }}>
+      <Tooltip title={title} placement='top' arrow>
+        <Card
+          sx={{ width: 345, height: 370 }}
+          py={2}
+          onClick={() => getRecipe()}
+        >
+          <CardMedia
+            component="img"
+            height="175"
+            image={renderImage()}
+            alt={title} />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div" textAlign={'center'}>
+              {title}
+            </Typography>
+            <Grid container px={2} pt={3} >
+              <Grid item xs={12} sm={6} md={4}
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  flexWrap: 'wrap',
+                  marginRight: 6,
+                  justifyContent: 'center'
+                }}>
+                <AccessTimeOutlinedIcon />
+                <Typography>{cookMinutes} minutes</Typography>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  flexWrap: 'wrap',
+                  justifyContent: 'center'
+                }}>
+                <Box sx={{ justifyContent: 'center' }}>
+                  <MenuBookOutlinedIcon />
+                </Box>
+                <Typography>{servings} servings</Typography>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Tooltip>
+    </CardActionArea>
   );
 }
