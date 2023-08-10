c786329aaec67142a84c7451a6332e735ec354e4
unclebabykern <coren.frankel@gmail.com>
2023-08-10 14:45:38 -0600

    Create psuedo-response object

---
+++ agile_aprons/src/main/java/learn/agileaprons/models/SpoonacularSearchResults.java
@@ -0,0 +1,15 @@
+package learn.agileaprons.models;
+
+import java.util.List;
+
+public class SpoonacularSearchResults {
+    private List<SpoonacularRecipe> results;
+
+    public List<SpoonacularRecipe> getResults() {
+        return results;
+    }
+
+    public void setResults(List<SpoonacularRecipe> results) {
+        this.results = results;
+    }
+}
