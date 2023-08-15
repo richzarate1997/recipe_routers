da9a5ef0e64473105c5da2df6a1367247e662b5b
unclebabykern <coren.frankel@gmail.com>
2023-08-15 11:20:29 -0600

    Comment out unused list and add redirect
    BASE_RECIPES is commented out currently, may no longer be required. Added useNavigate hook to redirect if a search gets no results.

--- clients/client-web/src/components/views/RecipeResults.jsx
+++ clients/client-web/src/components/views/RecipeResults.jsx
@@ -1,103 +1,103 @@
 import { Box, Grid, Typography } from "@mui/material";
-import PacmanLoader from "react-spinners/PacmanLoader";
-import RecipeCard from "../RecipeCard";
 import { useEffect, useState } from "react";
-import { searchRecipes } from "../../service/recipeApi";
+import { useParams, useNavigate } from "react-router-dom";
+import PacmanLoader from "react-spinners/PacmanLoader";
 import Errors from "../Errors";
-import { useParams } from "react-router-dom";
-import { findAllRecipes } from "../../service/recipeApi";
+import RecipeCard from "../RecipeCard";
+import { searchRecipes, findAllRecipes } from "../../service/recipeApi";
 
 
-const BASE_RECIPES = [
-  {
-    "id": 491786,
-    "image": "Ranch-BLT-Pasta-Salad-491786.jpg",
-    "cookMinutes": 45,
-    "servings": 8,
-    "sourceUrl": "https://www.cinnamonspiceandeverythingnice.com/ranch-blt-pasta-salad/",
-    "title": "Ranch BLT Pasta Salad"
-  },
+// const BASE_RECIPES = [
+//   {
+//     "id": 491786,
+//     "image": "Ranch-BLT-Pasta-Salad-491786.jpg",
+//     "cookMinutes": 45,
+//     "servings": 8,
+//     "sourceUrl": "https://www.cinnamonspiceandeverythingnice.com/ranch-blt-pasta-salad/",
+//     "title": "Ranch BLT Pasta Salad"
+//   },
 
-  {
-    "id": 492413,
-    "image": "https://spoonacular.com/recipeImages/Chicken-Gnocchi-Soup-(Olive-Garden-Copycat)-492413.jpg",
-    "cookMinutes": 65,
-    "servings": 6,
-    "sourceUrl": "http://www.cinnamonspiceandeverythingnice.com/olive-garden-chicken-and-gnocchi-soup-copycat/",
-    "title": "Chicken Gnocchi Soup (Olive Garden Copycat)"
-  },
+//   {
+//     "id": 492413,
+//     "image": "https://spoonacular.com/recipeImages/Chicken-Gnocchi-Soup-(Olive-Garden-Copycat)-492413.jpg",
+//     "cookMinutes": 65,
+//     "servings": 6,
+//     "sourceUrl": "http://www.cinnamonspiceandeverythingnice.com/olive-garden-chicken-and-gnocchi-soup-copycat/",
+//     "title": "Chicken Gnocchi Soup (Olive Garden Copycat)"
+//   },
 
-  {
-    "id": 909570,
-    "image": "soy-sauce-noodles-909570.jpg",
-    "cookMinutes": 30,
-    "servings": 2,
-    "sourceUrl": "http://www.loveandoliveoil.com/2015/03/soy-sauce-noodles.html",
-    "title": "Soy Sauce Noodles"
-  },
+//   {
+//     "id": 909570,
+//     "image": "soy-sauce-noodles-909570.jpg",
+//     "cookMinutes": 30,
+//     "servings": 2,
+//     "sourceUrl": "http://www.loveandoliveoil.com/2015/03/soy-sauce-noodles.html",
+//     "title": "Soy Sauce Noodles"
+//   },
 
-  {
-    "id": 667384,
-    "image": "Beef-orzo-skillet-667384.jpg",
-    "cookMinutes": 45,
-    "servings": 4,
-    "sourceUrl": "http://www.sheknows.com/food-and-recipes/articles/1027283/beef-orzo-skillet-recipe",
-    "title": "Beef orzo skillet"
-  },
+//   {
+//     "id": 667384,
+//     "image": "Beef-orzo-skillet-667384.jpg",
+//     "cookMinutes": 45,
+//     "servings": 4,
+//     "sourceUrl": "http://www.sheknows.com/food-and-recipes/articles/1027283/beef-orzo-skillet-recipe",
+//     "title": "Beef orzo skillet"
+//   },
 
-  {
-    "id": 709814,
-    "image": "roasted-ratatouille-pasta-709814.jpg",
-    "cookMinutes": 63,
-    "servings": 3,
-    "sourceUrl": "http://thewoksoflife.com/2014/03/ratatouille-pasta/",
-    "title": "Roasted Ratatouille Pasta"
-  },
+//   {
+//     "id": 709814,
+//     "image": "roasted-ratatouille-pasta-709814.jpg",
+//     "cookMinutes": 63,
+//     "servings": 3,
+//     "sourceUrl": "http://thewoksoflife.com/2014/03/ratatouille-pasta/",
+//     "title": "Roasted Ratatouille Pasta"
+//   },
 
-  {
-    "id": 750949,
-    "image": "mexican-macaroni-salad-750949.jpeg",
-    "cookMinutes": 60,
-    "servings": 12,
-    "sourceUrl": "http://www.foodnetwork.com/recipes/ree-drummond/mexican-macaroni-salad.html",
-    "title": "Mexican Macaroni Salad"
-  },
+//   {
+//     "id": 750949,
+//     "image": "mexican-macaroni-salad-750949.jpeg",
+//     "cookMinutes": 60,
+//     "servings": 12,
+//     "sourceUrl": "http://www.foodnetwork.com/recipes/ree-drummond/mexican-macaroni-salad.html",
+//     "title": "Mexican Macaroni Salad"
+//   },
 
-  {
-    "id": 718981,
-    "image": "cheeseburger-gnocchi-718981.jpg",
-    "cookMinutes": 45,
-    "servings": 4,
-    "sourceUrl": "http://www.kevinandamanda.com/recipes/dinner/cheeseburger-gnocchi.html",
-    "title": "Cheeseburger Gnocchi"
-  },
+//   {
+//     "id": 718981,
+//     "image": "cheeseburger-gnocchi-718981.jpg",
+//     "cookMinutes": 45,
+//     "servings": 4,
+//     "sourceUrl": "http://www.kevinandamanda.com/recipes/dinner/cheeseburger-gnocchi.html",
+//     "title": "Cheeseburger Gnocchi"
+//   },
 
-  {
-    "id": 864561,
-    "image": "mediterranean-pasta-salad-864561.jpg",
-    "cookMinutes": 25,
-    "servings": 8,
-    "sourceUrl": "http://littlespicejar.com/mediterranean-pasta-salad/",
-    "title": "Mediterranean Pasta Salad"
-  },
+//   {
+//     "id": 864561,
+//     "image": "mediterranean-pasta-salad-864561.jpg",
+//     "cookMinutes": 25,
+//     "servings": 8,
+//     "sourceUrl": "http://littlespicejar.com/mediterranean-pasta-salad/",
+//     "title": "Mediterranean Pasta Salad"
+//   },
 
-  {
-    "id": 494242,
-    "image": "Pumpkin---Ricotta-Stuffed-Shells-494242.jpg",
-    "cookMinutes": 45,
-    "servings": 10,
-    "sourceUrl": "http://www.loveandoliveoil.com/2013/02/pumpkin-ricotta-stuffed-shells.html",
-    "title": "Pumpkin & Ricotta Stuffed Shells"
-  },
+//   {
+//     "id": 494242,
+//     "image": "Pumpkin---Ricotta-Stuffed-Shells-494242.jpg",
+//     "cookMinutes": 45,
+//     "servings": 10,
+//     "sourceUrl": "http://www.loveandoliveoil.com/2013/02/pumpkin-ricotta-stuffed-shells.html",
+//     "title": "Pumpkin & Ricotta Stuffed Shells"
+//   },
 
-  {
-    "id": 725830,
-    "image": "bas-best-baked-ziti-725830.jpg",
-    "cookMinutes": 45,
-    "servings": 6,
-    "sourceUrl": "http://www.bonappetit.com/recipe/bas-best-baked-ziti",
-    "title": "BA’s Best Baked Ziti"
-  }]
+//   {
+//     "id": 725830,
+//     "image": "bas-best-baked-ziti-725830.jpg",
+//     "cookMinutes": 45,
+//     "servings": 6,
+//     "sourceUrl": "http://www.bonappetit.com/recipe/bas-best-baked-ziti",
+//     "title": "BA’s Best Baked Ziti"
+//   }
+// ]
 
 const styles = {
   loader: {
@@ -113,6 +113,7 @@ function Recipe() {
   const [recipes, setRecipes] = useState(null);
   const [errors, setErrors] = useState([]);
   const { param } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (param) {
@@ -130,7 +131,6 @@ function Recipe() {
         })
         .catch(err => setErrors(err));
     }
-    // console.log(recipes);
   }, [param]);
 
   if (recipes === null) {
@@ -142,7 +142,11 @@ function Recipe() {
   }
 
   if (recipes.length === 0) {
-    <Typography variant="p">No recipes to display.</Typography>
+    return navigate("/notfound", {
+      state: {
+        msg: `No recipes found searching: ${param}. Try something else.`
+      }
+    })
   }
 
   return (
@@ -154,7 +158,7 @@ function Recipe() {
               id={recipe.id}
               imageUrl={recipe?.imageUrl || recipe?.image} // anticipate blob image type
               title={recipe.title}
-              cookMinutes={recipe?.cookMinutes}
+              cookMinutes={recipe.cookMinutes}
               servings={recipe.servings}
             />
           </Grid>
