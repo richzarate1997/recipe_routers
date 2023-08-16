5e24c04c06a4b055469fd2a7c415e9057bc8e078
unclebabykern <coren.frankel@gmail.com>
2023-08-16 15:57:57 -0600

    Reformat tabs & fix typo in empty user object

--- clients/client-web/src/components/views/Profile.jsx
+++ clients/client-web/src/components/views/Profile.jsx
@@ -6,48 +6,48 @@ import MyRecipes from '../MyRecipes';
 
 
 const Profile = ({ appUser }) => {
-    const [user, setUser] = useState({
-        displayName: '',
-        metric: false,
-        myLists: [],
-        MyRecipes: [],
-        myFavorites: []
-    });
+  const [user, setUser] = useState({
+    displayName: '',
+    metric: false,
+    myLists: [],
+    myRecipes: [],
+    myFavorites: []
+  });
 
-    useEffect(() => {
-        findUser()
-            .then(data => {
-                setUser(data)
-                console.log(data);
-            })
-            .catch(err => console.log(err));
-    }, [appUser]);
+  useEffect(() => {
+    findUser()
+      .then(data => {
+        setUser(data)
+        console.log(data);
+      })
+      .catch(err => console.log(err));
+  }, [appUser]);
 
-    const label = { inputProps: { 'aria-label': 'Metric/Imperial units preference toggle' } }
-    return (
-        <Box mx={5} sx={{ paddingTop: 2 }} >
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Typography variant='h3'>Hey there, {user.displayName}</Typography>
-                <FormControlLabel
-                    control={<Switch {...label}
-                        checked={user.metric}
-                        color="default" />
-                    }
-                    label="Metric / Imperial"
-                    labelPlacement='top'
-                />
-            </div>
-            <Grid container spacing={2} mx='auto' my={2}>
-                <Grid item xs={12} sm={6} md={4}>
-                    <MyRecipes recipes={user.myRecipes} favorites={user.myFavorites}/>
-                </Grid>
-                <Grid item xs={12} sm={6} md={8}>
-                    <Typography variant='h5'>My Grocery List</Typography>
-                    {user && <GroceryListList />}
-                </Grid>
-            </Grid>
-        </Box>
-    )
+  const label = { inputProps: { 'aria-label': 'Metric/Imperial units preference toggle' } }
+  return (
+    <Box mx={5} sx={{ paddingTop: 2 }} >
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography variant='h3'>Hey there, {user.displayName}</Typography>
+        <FormControlLabel
+          control={<Switch {...label}
+            checked={user.metric}
+            color="default" />
+          }
+          label="Metric / Imperial"
+          labelPlacement='top'
+        />
+      </div>
+      <Grid container spacing={2} mx='auto' my={2}>
+        <Grid item xs={12} sm={6} md={4}>
+          <MyRecipes recipes={user.myRecipes} favorites={user.myFavorites} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={8}>
+          <Typography variant='h5'>My Grocery List</Typography>
+          {user && <GroceryListList />}
+        </Grid>
+      </Grid>
+    </Box>
+  )
 }
 
 export default Profile
