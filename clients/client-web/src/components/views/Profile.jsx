234a706b960bc389a2a082844bee44d4b107672e
unclebabykern <coren.frankel@gmail.com>
2023-08-17 15:18:47 -0600

    Add recipe images and delete-success msg

--- clients/client-web/src/components/views/Profile.jsx
+++ clients/client-web/src/components/views/Profile.jsx
@@ -1,18 +1,34 @@
-import { findUser, update } from '../../service/userApi';
-import { Box, FormControlLabel, Grid, Typography, Switch } from '@mui/material';
+import {
+  Alert, Box, FormControlLabel,
+  Grid, Switch, Typography
+} from '@mui/material';
 import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import GroceryListList from '../GroceryListList';
 import MyRecipes from '../MyRecipes';
+import { findUser, update } from '../../service/userApi';
 
+const EMPTY_USER = {
+  displayName: '',
+  metric: false,
+  myLists: [],
+  myRecipes: [],
+  myFavorites: []
+};
+
+const styles = {
+  alert: {
+    borderRadius: '20px 20px',
+    margin: 'auto',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+}
 
 const Profile = ({ appUser }) => {
-  const [user, setUser] = useState({
-    displayName: '',
-    metric: false,
-    myLists: [],
-    myRecipes: [],
-    myFavorites: []
-  });
+  const [user, setUser] = useState(EMPTY_USER);
+  const location = useLocation();
 
   useEffect(() => {
     findUser()
@@ -28,6 +44,13 @@ const Profile = ({ appUser }) => {
     <Box mx={5} sx={{ paddingTop: 2 }} >
       <div style={{ display: 'flex', justifyContent: 'space-between' }}>
         <Typography variant='h3'>Hey there, {user.displayName}</Typography>
+        {location.state &&
+          <Alert severity="success" sx={styles.alert}>
+            <Typography variant="subtitle1">
+              {location.state.msg}
+            </Typography>
+          </Alert>
+        }
         <FormControlLabel
           control={<Switch {...label}
             checked={user.metric}
@@ -38,14 +61,15 @@ const Profile = ({ appUser }) => {
         />
       </div>
       <Grid container spacing={2} mx='auto' my={2}>
-        <Grid item xs={12} sm={6} md={4}>
+        <Grid item xs={12} sm={10} md={4}>
           <MyRecipes recipes={user.myRecipes} favorites={user.myFavorites} />
         </Grid>
-        <Grid item xs={12} sm={6} md={8}>
+        <Grid item xs={12} sm={4} md={4}>
           <Typography variant='h5'>My Grocery List</Typography>
           {user && <GroceryListList />}
         </Grid>
       </Grid>
+
     </Box>
   )
 }
