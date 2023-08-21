acd9e4b67766d9635e5a80dd36874aef4851d4a9
unclebabykern <coren.frankel@gmail.com>
2023-08-21 11:58:55 -0600

    Reformat tabs

--- clients/client-web/src/components/Errors.jsx
+++ clients/client-web/src/components/Errors.jsx
@@ -3,24 +3,24 @@ import { useState, useEffect } from "react";
 
 
 function Errors({ errs }) {
-    const [errors, setErrors] = useState([]);
+  const [errors, setErrors] = useState([]);
 
-    useEffect(() => {
-        setErrors(errs);
-    }, [errs]);
-    return (
-        <>
+  useEffect(() => {
+    setErrors(errs);
+  }, [errs]);
+  return (
+    <>
 
-            <Alert severity="error" style={{ display: 'flex', alignItems: 'center', marginTop: '20px' }}>
-                <List>
-                    {typeof errors === 'object'
-                        ? errors.map(err => <ListItem key={err}><ListItemText primary={err} /></ListItem>)
-                        : <ListItem><ListItemText primary={errors} /></ListItem>
-                    }
-                </List>
-            </Alert>
-        </>
-    );
+      <Alert severity="error" style={{ display: 'flex', alignItems: 'center', marginTop: '20px' }}>
+        <List>
+          {typeof errors === 'object'
+            ? errors.map(err => <ListItem key={err}><ListItemText primary={err} /></ListItem>)
+            : <ListItem><ListItemText primary={errors} /></ListItem>
+          }
+        </List>
+      </Alert>
+    </>
+  );
 }
 
 export default Errors;
