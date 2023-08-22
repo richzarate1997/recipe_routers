5283ad46c12419aa05a7c1e72c6b59b5c7a649c9
unclebabykern <coren.frankel@gmail.com>
2023-08-22 12:52:40 -0600

    Reformat quotation consistency

--- clients/client-web/src/App.js
+++ clients/client-web/src/App.js
@@ -1,22 +1,22 @@
-import { ThemeProvider, createTheme } from "@mui/material"
-import { useState, useEffect, useCallback } from "react";
-import { Route, Routes, BrowserRouter as Router, Navigate } from "react-router-dom";
+import { ThemeProvider, createTheme } from '@mui/material'
+import { useState, useEffect, useCallback } from 'react';
+import { Route, Routes, BrowserRouter as Router, Navigate } from 'react-router-dom';
 import './App.css';
-import Home from "./components/views/Home";
-import ResponsiveAppBar from "./components/ResponsiveAppBar";
-import Recipe from "./components/views/RecipeResults";
-import ShowRecipe from "./components/views/ShowRecipe";
-import About from "./components/views/About"
-import Profile from "./components/views/Profile";
-import Login from "./components/views/Login";
-import Footer from "./components/Footer";
-import GroceryListForm from "./components/forms/GroceryListForm";
-import RecipeForm from "./components/forms/RecipeForm";
-import NotFound from "./components/views/NotFound";
-import AddGroceries from "./components/AddGroceries";
-import FavoriteRecipesList from "./components/FavoriteRecipesList";
-import AuthContext from "./contexts/AuthContext";
-import { refreshToken, signOut } from "./service/authApi";
+import Home from './components/views/Home';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
+import Recipe from './components/views/RecipeResults';
+import ShowRecipe from './components/views/ShowRecipe';
+import About from './components/views/About'
+import Profile from './components/views/Profile';
+import Login from './components/views/Login';
+import Footer from './components/Footer';
+import GroceryListForm from './components/forms/GroceryListForm';
+import RecipeForm from './components/forms/RecipeForm';
+import NotFound from './components/views/NotFound';
+import AddGroceries from './components/AddGroceries';
+import FavoriteRecipesList from './components/FavoriteRecipesList';
+import AuthContext from './contexts/AuthContext';
+import { refreshToken, signOut } from './service/authApi';
 
 
 const EMPTY_USER = {
@@ -77,7 +77,7 @@ function App() {
         contrastText: '#fff'
       },
       secondary: {
-        main: "#FEAE65",
+        main: '#FEAE65',
       },
       warning: {
         main: '#CA5953'
@@ -101,23 +101,23 @@ function App() {
         <Router>
           <ResponsiveAppBar />
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/recipes" element={<Recipe />} />
-            <Route path="/search/:param" element={<Recipe />} />
-            <Route path="/recipe/:id" element={<ShowRecipe userId={user.appUserId} />} />
-            <Route path="/add/ingredient" element={<AddGroceries />} />
-            <Route path="/new/recipe" element={mayRedirect(<RecipeForm />, <Navigate to="/recipes" />)} />
-            <Route path="/edit/recipe/:id" element={mayRedirect(<RecipeForm userId={auth.user.appUserId}/>, <Navigate to="/recipes" />)} />
-            <Route path="/add/grocerylist" element={mayRedirect(<GroceryListForm />, <Navigate to="/recipes" />)} />
-            <Route path="/myfavorites" element={mayRedirect(<FavoriteRecipesList />, <Navigate to="/recipes" />)} />
-            <Route path="/about" element={<About />} />
-            <Route path="/profile" element={mayRedirect(<Profile appUser={auth.user} />, <Navigate to='/' />)} />
-            <Route path="/login" element={mayRedirect(<Navigate to='/profile' />, <Login purpose="Sign In" />)} />
-            <Route path="/register" element={
-              mayRedirect(<Navigate to='/profile' />, <Login purpose="Register" isRegister={true} />)
+            <Route path='/' element={<Home />} />
+            <Route path='/recipes' element={<Recipe />} />
+            <Route path='/search/:param' element={<Recipe />} />
+            <Route path='/recipe/:id' element={<ShowRecipe userId={user.appUserId} />} />
+            <Route path='/add/ingredient' element={<AddGroceries />} />
+            <Route path='/new/recipe' element={mayRedirect(<RecipeForm />, <Navigate to='/recipes' />)} />
+            <Route path='/edit/recipe/:id' element={mayRedirect(<RecipeForm userId={auth.user.appUserId}/>, <Navigate to='/recipes' />)} />
+            <Route path='/add/grocerylist' element={mayRedirect(<GroceryListForm />, <Navigate to='/recipes' />)} />
+            <Route path='/myfavorites' element={mayRedirect(<FavoriteRecipesList />, <Navigate to='/recipes' />)} />
+            <Route path='/about' element={<About />} />
+            <Route path='/profile' element={mayRedirect(<Profile appUser={auth.user} />, <Navigate to='/' />)} />
+            <Route path='/login' element={mayRedirect(<Navigate to='/profile' />, <Login purpose='Sign In' />)} />
+            <Route path='/register' element={
+              mayRedirect(<Navigate to='/profile' />, <Login purpose='Register' isRegister={true} />)
             } />
-            <Route path="/notfound" element={<NotFound />} />
-            <Route path="*" element={<Navigate to={'/notfound'} />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='*' element={<Navigate to={'/notfound'} />} />
           </Routes>
           <Footer />
         </Router>
