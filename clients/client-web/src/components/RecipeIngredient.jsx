2f6c95d175c90a13336cd53283db092aeb874260
unclebabykern <coren.frankel@gmail.com>
2023-08-22 11:47:37 -0600

    Reformat ingredient styles on step 3

--- clients/client-web/src/components/RecipeIngredient.jsx
+++ clients/client-web/src/components/RecipeIngredient.jsx
@@ -1,65 +1,86 @@
-import { Grid, Stack, TextField, Select, MenuItem, Typography, OutlinedInput, Tooltip } from '@mui/material';
+import {
+  Avatar, Grid, MenuItem,
+  OutlinedInput, Select,
+  TextField, Tooltip,
+  Typography
+} from '@mui/material';
 
+const styles = {
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  name: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+}
 
 const RecipeIngredient = ({ units, ingredient, onChange }) => {
 
-    const handleChange = (e) => {
-        const nextRecipeIngredient = { ...ingredient };
-        let nextValue = e.target.value;
-        if (e.target.type === 'number') {
-            nextRecipeIngredient[e.target.name] = parseFloat(nextValue, 10);
-        } else {
-            nextValue = parseInt(nextValue, 10);
-            nextRecipeIngredient[e.target.name] = units.find(u => u.id === nextValue);
-        }
-        onChange(nextRecipeIngredient);
+  const handleChange = (e) => {
+    const nextRecipeIngredient = { ...ingredient };
+    let nextValue = e.target.value;
+    if (e.target.type === 'number') {
+      nextRecipeIngredient[e.target.name] = parseFloat(nextValue, 10);
+    } else {
+      nextValue = parseInt(nextValue, 10);
+      nextRecipeIngredient[e.target.name] = units.find(u => u.id === nextValue);
     }
+    onChange(nextRecipeIngredient);
+  }
 
-    return (
-        <Grid container rowSpacing={2} sx={{ margin: 'auto' }} justifyContent={'center'}>
-            <Grid item xs={2} sm={2} md={2} lg={2}>
-                <TextField
-                    label="Quantity"
-                    type="number"
-                    name="quantity"
-                    size='small'
-                    InputProps={{ inputProps: { step: "0.5" } }}
-                    required
-                    value={ingredient.quantity}
-                    onChange={handleChange}
-                    sx={{ width: '5rem' }}
-                />
-            </Grid>
-            <Grid item xs={2} sm={2} md={2} lg={2}>
-                <Select
-                    required
-                    displayEmpty
-                    value={ingredient.unit.id}
-                    name="unit"
-                    size="small"
-                    width='20%'
-                    onChange={handleChange}
-                    input={<OutlinedInput />}
-                    inputProps={{ 'aria-label': 'Without label' }}
-                    sx={{ width: '5rem' }}
-                >
-                    <MenuItem disabled value='0'>
-                        Unit
-                    </MenuItem>
-                    {units.length > 0 && units.map((unit) => (
-                        <MenuItem key={unit.id} value={unit.id}>
-                            <Tooltip title={unit.name || ingredient.ingredient.name}>
-                                {unit.abbreviation || ingredient.ingredient.name}
-                            </Tooltip>
-                        </MenuItem>
-                    ))}
-                </Select>
-            </Grid>
-            <Grid item xs={3} sm={3} md={4} lg={4}>
-                <Typography variant="body1" p={1} textAlign={'left'}>{ingredient.ingredient.name}</Typography>
-            </Grid>
-        </Grid>
-    )
+  return (
+    <Grid container rowSpacing={2} columnGap={3} marginY={0.5} sx={styles.container}>
+      <Grid item xs={2} sm={4} sx={styles.name}>
+        <Typography variant='overline' p={1} >{ingredient.ingredient.name}</Typography>
+      </Grid>
+      <Grid item xs={2}>
+        <TextField
+          label='Quantity'
+          type='number'
+          name='quantity'
+          size='small'
+          inputProps={{ inputprops: { step: '0.5' } }}
+          required
+          value={ingredient.quantity}
+          onChange={handleChange}
+          sx={{ width: '5rem' }}
+        />
+      </Grid>
+      <Grid item xs={2}>
+        { units.length > 0 &&
+        <Select
+          defaultValue={0}
+          value={ingredient.unit.id}
+          name='unit'
+          size='small'
+          onChange={handleChange}
+          input={<OutlinedInput />}
+          sx={{ width: '5rem' }}
+        >
+          <MenuItem value={0}>
+            Unit
+          </MenuItem>
+          { units.map((unit) => (
+            <MenuItem key={unit.id} value={unit.id}>
+              <Tooltip title={unit.name}>
+                <>
+                  {unit.abbreviation || ingredient.ingredient.name}
+                </>
+              </Tooltip>
+            </MenuItem>
+          ))}
+        </Select>
+        }
+      </Grid>
+      <Grid item xs={2} sx={styles.name}>
+        <Avatar alt={ingredient.ingredient.name} src={ingredient.ingredient.imageUrl} />
+      </Grid>
+
+    </Grid>
+  )
 }
 
 export default RecipeIngredient
