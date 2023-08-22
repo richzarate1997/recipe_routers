5e25d7907cfc4a775678405aff2f6d66d0fd5e28
unclebabykern <coren.frankel@gmail.com>
2023-08-22 17:05:08 -0600

    Export helper functions

--- clients/client-web/src/components/modules/conversions.js
+++ clients/client-web/src/components/modules/conversions.js
@@ -1,3 +1,7 @@
+import DOMPurify from 'dompurify';
+import { toFraction } from 'fraction-parser';
+import parse from 'html-react-parser';
+
 export const renderCooktime = (cookMinutes) => {
   let result = "\n";
   if (cookMinutes >= 60) {
@@ -7,11 +11,25 @@ export const renderCooktime = (cookMinutes) => {
     result += cookMinutes % 60 + " min";
   }
   return result;
-}
+};
+
+export const renderIngredientText = (servings, ingredient) => {
+  if (ingredient.unit.name === 'serving' && ingredient.quantity === servings) {
+    if (ingredient.ingredient.aisle === 'Spices and Seasonings') return ingredient.ingredient.name + ' to taste'
+    return `${ingredient.quantity} serving(s) ${ingredient.ingredient.name} `;
+  } else {
+    return `${toFraction(ingredient.quantity, true)} ${ingredient.unit.name} ${ingredient.ingredient.name}`
+  }
+};
 
 export const hyphenate = (string) => {
   if (string.split(' ').length > 1){
     return string.split(' ').join('-');
   }
   return string;
-}
+};
+
+export const renderInstructionText = (instructions) => {
+  let cleanInstructions = DOMPurify.sanitize(instructions);
+  return parse(cleanInstructions);
+};
