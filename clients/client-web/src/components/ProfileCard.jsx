7ee136c1c273bfb91f18f9c37efd666dcd461cca
unclebabykern <coren.frankel@gmail.com>
2023-08-25 12:53:12 -0600

    Reformat code; reorganize imports

--- clients/client-web/src/components/ProfileCard.jsx
+++ clients/client-web/src/components/ProfileCard.jsx
@@ -1,29 +1,29 @@
-import { Card, CardActionArea, CardMedia, CardContent, Typography, Divider } from "@mui/material";
+import {
+  Card, CardContent,
+  CardMedia, Divider,
+  Typography
+} from "@mui/material";
 
-
-function ProfileCard({imgUrl, imgDesc, name, description}) {
-    return (
-        <Card sx={{ maxWidth: 350, minHeight: 535}}>
-            
-                <CardMedia
-                    component="img"
-                    height="360"
-                    src={imgUrl}
-                    alt={imgDesc}
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {name}
-                    </Typography>
-                    <Divider ></Divider>
-                    <Typography variant="body2" color="text.secondary" pt={2}>
-                        {description}
-                    </Typography>
-                
-                </CardContent>
-            
-        </Card>
-    )
+function ProfileCard({ imgUrl, imgDesc, name, description }) {
+  return (
+    <Card sx={{ maxWidth: 350, minHeight: 535 }}>
+      <CardMedia
+        component="img"
+        height="360"
+        src={imgUrl}
+        alt={imgDesc}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+        <Divider ></Divider>
+        <Typography variant="body2" color="text.secondary" pt={2}>
+          {description}
+        </Typography>
+      </CardContent>
+    </Card>
+  )
 }
 
 export default ProfileCard;
